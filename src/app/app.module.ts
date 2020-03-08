b790b8d181201389471076a4cd7fab63fb0bb2f5
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-03-08 23:23:48 +0100

    Cleaned header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import {ButtonModule} from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Button } from 'protractor';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
