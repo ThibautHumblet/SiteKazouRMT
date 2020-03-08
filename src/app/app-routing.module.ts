6dca69e8ce5ed327c8c3d0f7eb34a0c3e5823c05
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-03-08 23:57:15 +0100

    Cleaned index component + added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { flush } from '@angular/core/testing';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,26 @@ import {CardModule} from 'primeng/card';
 import {ButtonModule} from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Button } from 'protractor';
 import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    RouterModule.forRoot([
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
